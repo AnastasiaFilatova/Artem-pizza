6d0fbdeef0de1b41c5e3d7155f6343e646d5269d
Anastasia Filatova <anastasia.n.filatova@gmail.com>
2020-11-11 10:45:18 +0000

    Create a method for counting total price. Fix misspelled sauces word.

--- src/PizzaKit.js
+++ src/PizzaKit.js
@@ -7,36 +7,37 @@ class PizzaKit extends React.Component {
     super(props);
     this.state = {
       sizes: [
-        { id: 0, value: "30cm", isChecked: true },
-        { id: 1, value: "35cm", isChecked: false }],
-      bases:  [
-        { id: 0, value:  "Тонкое", isChecked: true },
-        { id: 1, value: "Пышное", isChecked: false }],
-      sauses: [
-        { id: 0, value:  "Томатный", isChecked: true },
-        { id: 1, value: "Белый", isChecked: false },
-        { id: 2, value: "Острый", isChecked: false }],
+        { id: 0, value: "30cm", isChecked: true, price: 200 },
+        { id: 1, value: "35cm", isChecked: false, price: 250 }],
+      bases: [
+        { id: 0, value: "Тонкое", isChecked: true, price: 0 },
+        { id: 1, value: "Пышное", isChecked: false, price: 0 }],
+      sauces: [
+        { id: 0, value: "Томатный", isChecked: true, price: 0 },
+        { id: 1, value: "Белый", isChecked: false, price: 0 },
+        { id: 2, value: "Острый", isChecked: false, price: 0 }],
       cheeses: [
-        { id: 0, value: "Моцарелла", isChecked: false },
-        { id: 1, value: "Чеддер", isChecked: false },
-        { id: 2, value: "Дор Блю", isChecked: false }
+        { id: 0, value: "Моцарелла", isChecked: false, price: 29 },
+        { id: 1, value: "Чеддер", isChecked: false, price: 29 },
+        { id: 2, value: "Дор Блю", isChecked: false, price: 29 }
       ],
       vegetables: [
-        { id: 0, value: "Помидор", isChecked: false },
-        { id: 1, value: "Грибы", isChecked: false },
-        { id: 2, value: "Перец", isChecked: false },
-        { id: 3, value: "Ананасы", isChecked: false },
-        { id: 4, value: "Оливки", isChecked: false },
-        { id: 5, value: "Лук", isChecked: false },
-        { id: 6, value: "Брокколи", isChecked: false }
+        { id: 0, value: "Помидор", isChecked: false, price: 29 },
+        { id: 1, value: "Грибы", isChecked: false, price: 29 },
+        { id: 2, value: "Перец", isChecked: false, price: 29 },
+        { id: 3, value: "Ананасы", isChecked: false, price: 29 },
+        { id: 4, value: "Оливки", isChecked: false, price: 29 },
+        { id: 5, value: "Лук", isChecked: false, price: 29 },
+        { id: 6, value: "Брокколи", isChecked: false, price: 29 }
       ],
       meats: [
-        { id: 0, value: "Бекон", isChecked: false },
-        { id: 1, value: "Пепперони", isChecked: false },
-        { id: 2, value: "Ветчина", isChecked: false },
-        { id: 3, value: "Курица", isChecked: false },
-        { id: 4, value: "Салями", isChecked: false }
-      ]
+        { id: 0, value: "Бекон", isChecked: false, price: 29 },
+        { id: 1, value: "Пепперони", isChecked: false, price: 29 },
+        { id: 2, value: "Ветчина", isChecked: false, price: 29 },
+        { id: 3, value: "Курица", isChecked: false, price: 29 },
+        { id: 4, value: "Салями", isChecked: false, price: 29 }
+      ],
+      totalPrice: [0]
     };
 
     this.handleItemChange = this.handleItemChange.bind(this);
@@ -64,10 +65,34 @@ class PizzaKit extends React.Component {
 
   onSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state);
-  };
+
+    var totalPrice = 0;
+    let items_state = this.state;
+
+    // Iterating through object of objects
+
+    Object.keys(items_state).forEach(key => {
+      console.log("key ", key, "value ", items_state[key])
+      items_state[key].forEach(item => {
+        if (item.isChecked) {
+          totalPrice += item.price;
+        }
+      })
+    })
+
+    this.setState({ totalPrice: [totalPrice] })
+    console.log("Total price: ", totalPrice);
+  }
 
   render() {
+    let button;
+
+    if (this.state.totalPrice[0] === 0) {
+      button = <button type="submit">Заказать за 200 руб.</button>;
+    } else {
+      button = <button type="submit">Заказать за {this.state.totalPrice[0]} руб.</button>;
+    }
+
     return (
       <div className="PizzaKit">
         <form onSubmit={this.onSubmit}>
@@ -75,31 +100,31 @@ class PizzaKit extends React.Component {
             <label>
               Размер
               {
-                  this.state.sizes.map((size) => {
-                    return (<RadioInput handleItemChange={this.handleItemChange} name="sizes" {...size} />)
-                  })
-                }
-              </label>
+                this.state.sizes.map((size) => {
+                  return (<RadioInput handleItemChange={this.handleItemChange} name="sizes" {...size} />)
+                })
+              }
+            </label>
           </div>
           <div className="radio">
             <label>
               Тесто
               {
-                  this.state.bases.map((base) => {
-                    return (<RadioInput handleItemChange={this.handleItemChange} name="bases" {...base} />)
-                  })
-                }
-              </label>
+                this.state.bases.map((base) => {
+                  return (<RadioInput handleItemChange={this.handleItemChange} name="bases" {...base} />)
+                })
+              }
+            </label>
           </div>
           <div className="radio">
             <label>
               Выберите соус
               {
-                  this.state.sauses.map((sause) => {
-                    return (<RadioInput handleItemChange={this.handleItemChange} name="sauses" {...sause} />)
-                  })
-                }
-              </label>
+                this.state.sauces.map((sauce) => {
+                  return (<RadioInput handleItemChange={this.handleItemChange} name="sauces" {...sauce} />)
+                })
+              }
+            </label>
             <div className="checkbox">
               <label>
                 Добавьте сыр
@@ -112,7 +137,7 @@ class PizzaKit extends React.Component {
             </div>
             <div className="checkbox">
               <label>
-              Добавьте овощи
+                Добавьте овощи
                 {
                   this.state.vegetables.map((vegetable) => {
                     return (<CheckBox handleItemChange={this.handleItemChange} name="vegetables" {...vegetable} />)
@@ -122,7 +147,7 @@ class PizzaKit extends React.Component {
             </div>
             <div className="checkbox">
               <label>
-              Добавьте мясо
+                Добавьте мясо
                 {
                   this.state.meats.map((meat) => {
                     return (<CheckBox handleItemChange={this.handleItemChange} name="meats" {...meat} />)
@@ -131,7 +156,7 @@ class PizzaKit extends React.Component {
               </label>
             </div>
           </div>
-          <button type="submit">Заказать за {'price here'}!</button>
+          {button}
         </form>
       </div>
     );
