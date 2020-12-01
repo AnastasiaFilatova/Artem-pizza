fa542838f31dada4791734257d5131036e7efa8e
Anastasia Filatova <anastasia.n.filatova@gmail.com>
2020-12-01 14:21:29 +0000

    Checkout page

---
+++ src/CheckoutPage/CheckoutPage.spec.js
@@ -0,0 +1,9 @@
+const { render, fireEvent } = require("@testing-library/react");
+const { CheckoutPage } = require("./CheckoutPage");
+
+describe("CheckoutPage", () => {
+  describe("Form renders correctly", () => {
+    it.todo("renders correctly");
+    it.todo("Page shows order data");
+  });
+});
