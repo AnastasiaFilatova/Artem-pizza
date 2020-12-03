0e552790415010c304f03d4092da6379efad51b9
Anastasia Filatova <anastasia.n.filatova@gmail.com>
2020-12-03 15:17:22 +0000

    Tests for the registration page

--- client/src/RegistrationPage/RegistrationPage.js
+++ client/src/RegistrationPage/RegistrationPage.js
@@ -12,21 +12,24 @@ const schema = yup.object().shape({
     .min(8, "Пароль должен состоять минимум из 8 символов"),
 });
 
-export const RegistrationPage = () => {
+export const RegistrationPage = ({ formSubmit }) => {
   const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(schema),
   });
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+    formSubmit(data);
+  };
 
   return (
     <>
       <h1>Регистрация</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Email</label>
-        <input type="text" name="email" ref={register} />
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" name="email" ref={register} />
         <p>{errors.email?.message}</p>
-        <label>Пароль</label>
-        <input type="text" name="password" ref={register} />
+        <label htmlFor="password">Пароль</label>
+        <input id="password" type="text" name="password" ref={register} />
         <p>{errors.password?.message}</p>
         <button>Зарегистрироваться</button>
       </form>
