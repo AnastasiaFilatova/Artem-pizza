5c9457c85da97ec6a375ca0883bec4bb5a25f508
Anastasia Filatova <anastasia.n.filatova@gmail.com>
2020-12-03 10:31:06 +0000

    Created admin app and moved pizza source code to the client folder

---
+++ admin/src/App.js
@@ -0,0 +1,55 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  price: yup
+    .number()
+    .transform((cv, ov) => (ov === "" ? undefined : cv))
+    .typeError("Цена должна быть числом")
+    .min(0, "Цена должна быть не меньше нуля")
+    .required("Цена - обязательное поле"),
+  name: yup.string().required("Название продукта обязательно"),
+  slug: yup.string().required("Введите идентификатор продукта"),
+  picture: yup.mixed(),
+});
+console.log(schema);
+
+function App() {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  console.log(errors);
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <label htmlFor="price">Цена:</label>
+        <input id="price" ref={register} type="tel" name="price" />
+        <p>{errors.price?.message}</p>
+      </div>
+      <div>
+        <label htmlFor="name">Название:</label>
+        <input id="name" ref={register} type="text" name="name" />
+        <p>{errors.name?.message}</p>
+      </div>
+      <div>
+        <label htmlFor="slug">Идентификатор:</label>
+        <input id="slug" ref={register} type="text" name="slug" />
+        <p>{errors.slug?.message}</p>
+      </div>
+      <div>
+        <label htmlFor="picture">Картинка:</label>
+        <input id="picture" ref={register} type="file" name="picture" />
+      </div>
+      <button>Отправить</button>
+    </form>
+  );
+}
+
+export default App;
