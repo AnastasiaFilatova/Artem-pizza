3b1a5a3ac097bca78dc5a9aec9e087d1c420d2f2
Anastasia Filatova <anastasia.n.filatova@gmail.com>
2021-01-04 14:03:16 +0000

    Get order's list only if user is authorised

--- client/src/OrdersListPage/OrdersListPage.js
+++ client/src/OrdersListPage/OrdersListPage.js
@@ -1,18 +1,24 @@
 import { getOrders, getIsLoading } from "../state/orders/selectors";
+import { Redirect } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchOrders } from "../state/orders/thunk";
+import { getAuthorisation } from "../state/authorisation/selectors";
 
 export const OrdersListPage = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(getIsLoading);
-  const state = useSelector((state) => state);
+  const isAuthorised = useSelector(getAuthorisation);
 
   useEffect(() => {
     dispatch(fetchOrders());
   }, []);
 
   const orders = useSelector(getOrders);
+
+  if (!isAuthorised) {
+    return <Redirect to="/login" />;
+  }
 
   if (isLoading) {
     return <>Заказы загружаются...</>;
@@ -21,6 +27,7 @@ export const OrdersListPage = () => {
   if (!orders) {
     return <h1>У вас нет заказов</h1>;
   }
+
   return (
     <>
       <h1>Заказы</h1>
